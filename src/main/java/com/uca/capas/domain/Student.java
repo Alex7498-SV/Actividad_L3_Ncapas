e7c30a642c0a96607ab83fd1916fa72c0ff71e84
Alex7498-SV <00068817@uca.edu.sv>
2020-04-28 17:11:49 -0600

    Creacion de nuevos html y cambios en el MainController

--- src/main/java/com/uca/capas/domain/Student.java
+++ src/main/java/com/uca/capas/domain/Student.java
@@ -3,51 +3,67 @@ package com.uca.capas.domain;
 public class Student {
 	
 	private String nombre;
-	private String apeliido;
+	private String apellido;
 	private String fEntrada;
 	private String carrera;
 	private Boolean  estado;
 	
+	public Student() {
+		
+	}
 	
 	
-	public Student(String nombre, String apeliido, String fEntrada, String carrera, Boolean estado) {
+	public Student(String nombre, String apellido, String fEntrada, String carrera, Boolean estado) {
 		super();
 		this.nombre = nombre;
-		this.apeliido = apeliido;
+		this.apellido = apellido;
 		this.fEntrada = fEntrada;
 		this.carrera = carrera;
 		this.estado = estado;
 	}
+
 	public String getNombre() {
 		return nombre;
 	}
+
 	public void setNombre(String nombre) {
 		this.nombre = nombre;
 	}
-	public String getApeliido() {
-		return apeliido;
+
+	public String getApellido() {
+		return apellido;
 	}
-	public void setApeliido(String apeliido) {
-		this.apeliido = apeliido;
+
+	public void setApellido(String apellido) {
+		this.apellido = apellido;
 	}
+
 	public String getfEntrada() {
 		return fEntrada;
 	}
+
 	public void setfEntrada(String fEntrada) {
 		this.fEntrada = fEntrada;
 	}
+
 	public String getCarrera() {
 		return carrera;
 	}
+
 	public void setCarrera(String carrera) {
 		this.carrera = carrera;
 	}
+
 	public Boolean getEstado() {
 		return estado;
 	}
+
 	public void setEstado(Boolean estado) {
 		this.estado = estado;
 	}
 	
+	public String delegateEstado() {
+		return estado ? "Activo": "inactivo";
+	}
 	
 }
