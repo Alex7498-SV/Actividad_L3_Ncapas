ee36c2fa5ffb4a7848b442f674660d02243a5479
Alex7498-SV <00068817@uca.edu.sv>
2020-04-27 18:15:11 -0600

    Man controler y dominio student creado

---
+++ src/main/java/com/uca/capas/controller/MainController.java
@@ -0,0 +1,17 @@
+package com.uca.capas.controller;
+
+import org.springframework.http.MediaType;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class MainController {
+	
+	
+	@GetMapping(path = "/ejemplo1", produces= MediaType.TEXT_PLAIN_VALUE)
+	@ResponseBody
+	public String ejemplo1() {
+		return "Bienvenidos\n"+"Programacion N-capas";
+	}
+}
--- src/main/java/com/uca/capas/domain/Student.java
+++ src/main/java/com/uca/capas/domain/Student.java
@@ -1,5 +1,42 @@
 package com.uca.capas.domain;
 
 public class Student {
-
+	
+	private String nombre;
+	private String apeliido;
+	private String fEntrada;
+	private String carrera;
+	private Boolean  estado;
+	public String getNombre() {
+		return nombre;
+	}
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+	public String getApeliido() {
+		return apeliido;
+	}
+	public void setApeliido(String apeliido) {
+		this.apeliido = apeliido;
+	}
+	public String getfEntrada() {
+		return fEntrada;
+	}
+	public void setfEntrada(String fEntrada) {
+		this.fEntrada = fEntrada;
+	}
+	public String getCarrera() {
+		return carrera;
+	}
+	public void setCarrera(String carrera) {
+		this.carrera = carrera;
+	}
+	public Boolean getEstado() {
+		return estado;
+	}
+	public void setEstado(Boolean estado) {
+		this.estado = estado;
+	}
+	
+	
 }
