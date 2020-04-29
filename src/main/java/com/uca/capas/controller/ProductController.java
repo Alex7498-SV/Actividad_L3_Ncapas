7972eceb086b0fb58518cbc4921a0c98e8bacac0
Alex7498-SV <00068817@uca.edu.sv>
2020-04-28 18:34:44 -0600

    correccion de errores en ProductController

---
+++ src/main/java/com/uca/capas/controller/ProductController.java
@@ -0,0 +1,41 @@
+package com.uca.capas.controller;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.servlet.ModelAndView;
+
+import com.uca.capas.domain.Product;
+
+@Controller
+public class ProductController {
+
+	private List<Product> productos = new ArrayList<Product>();
+	
+	@GetMapping("/comprarproducto")
+	public ModelAndView compraProducto() {
+		ModelAndView mav = new ModelAndView();
+		
+		productos.add(new Product(0, "alsjda", 8));
+		productos.add(new Product(1, "aqlwn", 1));
+		productos.add(new Product(2, "asdfjlnallña", 6));
+		
+		mav.setViewName("select");
+		mav.addObject("product", new Product());
+		mav.addObject("producto", productos);
+		
+		return mav;
+	}
+	
+	
+	@PostMapping("validar")
+	@ResponseBody
+	public String validar(Product product) {
+		return productos.get(product.getId()).getNombre();
+	}
+	
+}
